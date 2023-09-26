385d7635ce8331154bc6995f40f6c38686c44f00
Jorge <j0rg3e93@gmail.com>
2023-09-26 18:33:14 +0200

    Se agraga lo necesario para utilizar owl-carousel, se empieza a maquetar la pagina principal

--- src/js/myjs.js
+++ src/js/myjs.js
@@ -1,3 +1,15 @@
-$(document).ready(function ($) {
-  $(".owl-carousel").owlCarousel();
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    dots:false,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      
+    },
+  });
 });
