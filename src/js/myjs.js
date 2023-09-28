fd5408343348900aa43cfbdc15d14f07c5aca23c
Jorge <j0rg3e93@gmail.com>
2023-09-28 20:09:58 +0200

    Se agregan iconos en el footer, en la pagina de contacto se agregan estilos, para mejorar el diseño, en el resto de pagina se unifica el footer

--- src/js/myjs.js
+++ src/js/myjs.js
@@ -3,13 +3,15 @@ $(document).ready(function () {
     loop: true,
     margin: 0,
     responsiveClass: true,
-    dots:false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 5500,
+    navContainer: '.main-content .custom-nav',
     responsive: {
       0: {
         items: 1,
         nav: true,
       },
-      
     },
   });
 });
